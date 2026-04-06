5425c67e8465c410ed4a6c70d8905c0169071879
Antonio Lucas <133284222+LucasBr07@users.noreply.github.com>
2026-04-06 10:48:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
-function entrar(){
+function abrirLogin() {
+    document.getElementById("modalLogin").style.display = "flex";
+}
 
-alert("Sistema em desenvolvimento - Sprint 1")
+function fazerLogin() {
+    let login = document.getElementById("login").value;
+    let senha = document.getElementById("senha").value;
 
+    fetch("http://localhost:8080/usuarios/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ login, senha })
+    })
+    .then(res => res.json())
+    .then(data => {
+
+        if (data) {
+            document.getElementById("modalLogin").style.display = "none";
+            document.getElementById("app").style.display = "block";
+        } else {
+            alert("Login inválido!");
+        }
+
+    });
+}
+
+function abrirCadastro() {
+    document.getElementById("modalCadastro").style.display = "flex";
+}
+
+function fecharCadastro() {
+    document.getElementById("modalCadastro").style.display = "none";
+}
+
+function cadastrarUsuario() {
+    let nome = document.getElementById("nome").value;
+    let login = document.getElementById("novoLogin").value;
+    let senha = document.getElementById("novaSenha").value;
+
+    fetch("http://localhost:8080/usuarios/cadastrar", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ nome, login, senha })
+    })
+    .then(() => {
+        alert("Usuário cadastrado!");
+        fecharCadastro();
+    });
 }
