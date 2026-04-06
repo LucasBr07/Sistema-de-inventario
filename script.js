fa8048b9331d736573ee072509cf721a6dd7a15c
Antonio Lucas <133284222+LucasBr07@users.noreply.github.com>
2026-04-06 14:01:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function fazerLogin() {
     let login = document.getElementById("login").value;
     let senha = document.getElementById("senha").value;
 
-    fetch("http://localhost:8080/usuarios/login", {
+    fetch("http://inventario-backend-production-4a29.up.railway.app/usuarios/login", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({ login, senha })
