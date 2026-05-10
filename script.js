f575e1a53c6272fda18a905db96617febdc346b4
Antonio Lucas <133284222+LucasBr07@users.noreply.github.com>
2026-05-10 17:34:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function abrirLogin() {
 function fazerLogin() {
     let login = document.getElementById("login").value;
     let senha = document.getElementById("senha").value;
-
     fetch("https://inventario-backend-9oz3.onrender.com/usuarios/login", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
@@ -13,14 +12,12 @@ function fazerLogin() {
     })
     .then(res => res.json())
     .then(data => {
-
         if (data) {
             document.getElementById("modalLogin").style.display = "none";
             document.getElementById("app").style.display = "block";
         } else {
             alert("Login inválido!");
         }
-
     });
 }
 
@@ -32,14 +29,14 @@ function fecharCadastro() {
     document.getElementById("modalCadastro").style.display = "none";
 }
 
-const btn = document.querySelector("#modalCadastro button");
-    btn.textContent = "Cadastrando...";
-    btn.disabled = true;
-
 function cadastrarUsuario() {
     let nome = document.getElementById("nome").value;
     let login = document.getElementById("novoLogin").value;
     let senha = document.getElementById("novaSenha").value;
+
+    const btn = document.getElementById("btnCadastrar");
+    btn.textContent = "Cadastrando...";
+    btn.disabled = true;
 
     fetch("https://inventario-backend-9oz3.onrender.com/usuarios/cadastrar", {
         method: "POST",
@@ -49,5 +46,9 @@ function cadastrarUsuario() {
     .then(() => {
         alert("Usuário cadastrado!");
         fecharCadastro();
+    })
+    .finally(() => {
+        btn.textContent = "Cadastrar";
+        btn.disabled = false;
     });
 }
